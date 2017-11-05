84ac293b6538026489b2e91c145d04a2e74e3c0c
grisu48 <grisu42@gmail.com>
2017-11-05 16:21:11 +0100

    Upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Iceland-CampingCard-Locations
-Python script to extract the locations of the CampingCard.is to a GPX file
+
+This is a simple python script to extract the locations of the CampingCard.is to a GPX file.
+
+The provided GPX file is valid for 2017 and was used by an Iceland trip.
+
+Use at own risk and verify before you use it!
+
+https://www.campingcard.is/
+
+## Usage 
+
+    ./fetch_campingcard_gpx.py >CampingCard.is_Locations.gpx
+
+This will run the script and writes the GPX file to `CampingCard.is_Locations.gpx`
+ 
---
+++ fetch_campingcard_gpx.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+import requests
+
+
+if __name__ == "__main__" :
+	
+	# Download
+	
+	r = requests.get('https://www.campingcard.is/')
+	if r.status_code != 200 : raise ValueError("Http status code " + str(r.status_code))
+	html = str(r.text)
+	
+	objs = None
+	try :
+		
+		map_objects = html[html.find('var tjaldsvaedi_markers'):]
+		map_objects = map_objects.split('\n')
+		map_objects = map_objects[1]
+		
+		text = '[' + map_objects.strip()[:-1] + ']'
+		
+		# Fix broken json
+		def fix_json(text, value) :
+			key = value + ":"
+			ret = text.replace(key, '"' + value + '":')
+			return ret
+		text = text.replace("'", '"')
+		text = fix_json(text, "position")
+		text = fix_json(text, "lng")
+		text = fix_json(text, "lat")
+		text = fix_json(text, "map")
+		text = fix_json(text, "title")
+		text = fix_json(text, "url")
+		text = fix_json(text, "categories")
+		text = fix_json(text, "description")
+		text = text.replace(', "map": map', '')
+		
+		with open('CampingCard.is_Locations.json', 'w') as f_out :
+			f_out.write(text)
+			f_out.write('\n')
+		
+		objs = json.loads(text)
+	
+	except Exception as e:
+		sys.stderr.write("Error parsing: " + str(e) + "\n")
+		sys.exit(1)
+
+	# Print GPX file
+	print(
+'''<?xml version="1.0" encoding="UTF-8"?>
+<gpx version="1.0">
+	<name>Example gpx</name>''')
+
+#	<wpt lat="46.57638889" lon="8.89263889">
+#		<ele>2372</ele>
+#		<name>LAGORETICO</name>
+#	</wpt>
+		
+	for i in objs :
+		try :
+			title = i['title']
+			pos = i['position']
+			lat = float(pos['lat'])
+			lon = float(pos['lng'])
+			url = i['url']
+			desc = i['description']
+			cat =i['categories']
+			
+			#print(title,lat,lon,desc, cat)
+			print('    <wpt lat="' + str(lat) + '" lon="' + str(lon) + '">')
+			print('        <name>' + str(title) + '</name>')
+			print('        <desc>' + str(desc) + '</desc>')
+			print('        <url>' + str(url) + '</url>')
+			print('    </wpt>')
+		except Exception as e :
+			sys.stderr.write("Error parsing element " + str(i) + "\n")
+	print('</gpx>')
